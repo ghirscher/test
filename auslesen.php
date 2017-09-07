f385d3e8fe70de559c9933bde59923f20d50c3ce
ghirscher <31744766+ghirscher@users.noreply.github.com>
2017-09-07 21:49:45 +0200

    first test
    Testing GitHub

---
+++ auslesen.php
@@ -0,0 +1,35 @@
+<?php
+
+$xml3 = simplexml_load_file('test3.xml');
+
+var_dump($xml3);
+
+print "<br><br>";
+
+print "<br>Einzeln Artikel auslesen: ".$xml3->Preis[1]["Artikel"];
+
+foreach ($xml3->Preis AS $artikel) {
+	print "<br>Mein Artikel: ".$artikel["Artikel"]." mit Preis: ".$artikel["Preis"]." und Status: ".$artikel["Status"];
+}
+
+print "<br><br>------------------------<br><br>";
+
+
+$xml2 = simplexml_load_file('test2.xml');
+
+var_dump($xml2);
+
+print "<br>STATUS: ".$xml2["Status"];
+
+print "<br><br>------------------------<br><br>";
+
+if (file_exists('test.xml')) {
+	
+	$xml = simplexml_load_file('test.xml');
+
+	foreach($xml->product AS $product) print "<br>Productnumbrs: ".$product->belboonproductnumber;
+
+}
+else exit('Konnte Datei nicht laden.');
+
+?>
